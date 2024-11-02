d87e42be9f3f69caa971b39ccaf1b35fc0f984b0
Plocks <130354934+Plocks2023@users.noreply.github.com>
2024-11-02 11:00:17 +0100

    Update client.lua

--- copy_coords/client/client.lua
+++ copy_coords/client/client.lua
@@ -1,8 +1,11 @@
+-- Notification
 ShowNotification = function(message)
     BeginTextCommandThefeedPost('STRING')
     AddTextComponentSubstringPlayerName(message)
     EndTextCommandThefeedPostTicker(0, 1)
 end
+
+--CMD 1
 
 RegisterCommand('copycoords', function()
     local coords= GetEntityCoords(PlayerPedId())
@@ -10,11 +13,15 @@ RegisterCommand('copycoords', function()
     ShowNotification('Koordinaten Kopiert!')
 end)
 
+--CMD 2
+
 RegisterCommand('copycoords2', function()
     local coords,heading = GetEntityCoords(PlayerPedId()), GetEntityHeading(PlayerPedId())
     lib.setClipboard('' .. vector3(coords.x, coords.y, coords.z) .. ' heading = ' .. heading)
     ShowNotification('Koordinaten Kopiert!')
  end)
+
+--CMD 3
 
 RegisterCommand('copycoords3', function()
     local coords,heading = GetEntityCoords(PlayerPedId()), GetEntityHeading(PlayerPedId())
@@ -22,11 +29,15 @@ RegisterCommand('copycoords3', function()
     ShowNotification('Koordinaten Kopiert!')
 end)
 
+--CMD 4
+
 RegisterCommand('copycoords4', function()
     local coords,heading = GetEntityCoords(PlayerPedId()), GetEntityHeading(PlayerPedId())
     lib.setClipboard('{ x = '.. coords.x ..', y = '..coords.y..', z = '.. coords.z ..', rot = '.. heading ..'}')
     ShowNotification('Koordinaten Kopiert!')
 end)
+
+--CMD 5
 
 RegisterCommand('copycoords5', function()
     local coords= GetEntityCoords(PlayerPedId())
