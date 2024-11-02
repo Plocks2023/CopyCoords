f1ba0779ee4891d7f7abc88028c6e7832862675d
Plocks <130354934+Plocks2023@users.noreply.github.com>
2024-11-02 10:59:08 +0100

    Update fxmanifest.lua

--- copy_coords/fxmanifest.lua
+++ copy_coords/fxmanifest.lua
@@ -4,13 +4,13 @@ game { 'gta5' }
 lua54 'yes'
 
 author 'itsplocks'
-description 'Copy Coords CMD by SVS - Scripts'
+description 'Copy Coords CMD by SC - Scripts'
 version '1.0.0'
-
-client_script {
-    'client/client.lua'
-}
 
 shared_scripts {
     '@ox_lib/init.lua',
+}
+
+client_script {
+    'client/client.lua'
 }
